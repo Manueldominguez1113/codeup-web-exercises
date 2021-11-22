7201529f4d436b306790691c9a4e3b4e3b703087
Manuel Dominguez <manuel.dominguez1113@gmail.com>
2021-11-22 15:08:15 -0600

    added new problems and completed exercises from over the weekend

--- working-with-types.js
+++ working-with-types.js
@@ -1,3 +1,87 @@
+var a = 1;
+var b = a++;
+var c = ++a;
+console.log("a is " + a +"  and b is " + b + " and C is " + c);
+// what is the value of a, b, and c?
+
+
+var d = "hello";
+var e = false;
+
+d++;
+e++;
+console.log(d + "and e is " + e);
+
+
+var perplexed; // perplexed is undefined (no value is assigned)
+perplexed + 2;
+console.log(perplexed);
+
+var price = 2.7;
+console.log(price.toFixed(2));
+
+var price = "2.7";
+price = parseInt(price)
+console.log(price.toFixed(2));
+
+console.log(isNaN(0));
+
+console.log(isNaN(1));
+
+console.log(isNaN(""));
+
+console.log(isNaN("string"));
+
+console.log(isNaN("0"));
+
+console.log(isNaN("1"));
+
+console.log(isNaN("3.145"));
+
+console.log(isNaN(Number.MAX_VALUE));
+
+console.log(isNaN(Infinity));
+
+console.log(isNaN("true"));
+
+console.log(isNaN(true));
+
+console.log(isNaN("false"));
+
+console.log(isNaN(false));
+
+// to illustrate why the isNaN() function is needed:
+console.log(NaN == NaN);
+
+console.log(!true);
+
+console.log(!false);
+
+console.log(!!true);
+
+console.log(!!false);
+
+console.log(!!0);
+
+console.log(!!-0);
+
+console.log(!!1);
+
+console.log(!!-1);
+
+console.log(!!0.1);
+
+console.log(!!"hello");
+
+console.log(!!"");
+
+console.log(!!'');
+
+console.log(!!"false");
+
+console.log(!!"0");
+
+
 
 let x = (3 + 5 + 1) * 3;
 
