1780869ade6722faa46a693fb6e58e6491ae954b
Manuel Dominguez <manuel.dominguez1113@gmail.com>
2021-12-05 00:48:28 -0600

    completed github & welcome & forms html. created a thankyou response page for el formo

--- Practiceexercisesdaily/practice12-3.js
+++ Practiceexercisesdaily/practice12-3.js
@@ -4,5 +4,4 @@ let foods=["spaghetti","lasagna","pie","steak","chicken wings"];
 //concat give your foods ratings by concat
 
 
-Object.defineProperty(foods[0],"Rating"){value: "5 stars"}
 console.log(foods)
