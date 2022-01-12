c5876226e3e890229ef3d4c289b27ac2a35a9a67
Manuel Dominguez <manuel.dominguez1113@gmail.com>
2022-01-12 10:43:24 -0600

    cupdate

--- js/dom-lecture.js
+++ js/dom-lecture.js
@@ -1,1 +1,27 @@
-mv
+let weather = prompt("what is the weather like today?", "");
+let ourParagraph = document.getElementById('textContent');
+switch(weather){
+    case 'rainy':
+        ourParagraph.textContent = "today it is rainy, enjoy the weather!"
+        break;
+    case 'cloudy':
+        ourParagraph.textContent = "today it is cloudy. maybe it'll rain?"
+        break;
+    case 'sunny':
+        ourParagraph.textContent = "today it is sunny. lets go outside!"
+        break;
+    case 'cold':
+        ourParagraph.textContent = "today it is cold. maybe take a warm jacket!"
+        break;
+    case 'warm':
+        ourParagraph.textContent = "its warm outside today. lets go to the beach!"
+        break;
+    case 'hail':
+        ourParagraph.textContent = "its hailing!! stay inside!"
+        break;
+    case 'tornadoes':
+        ourParagraph.textContent = "a TORNADO??? take cover!"
+        break;
+    default:
+        ourParagraph.textContent = "sorry, I did not understand the input!"
+}
