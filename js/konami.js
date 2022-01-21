0ab7ed94b5529e5e7a0b0c241fd361aaf95673b5
Manuel Dominguez <manuel.dominguez1113@gmail.com>
2022-01-21 11:13:08 -0600

    moved js and css out of html. cleanup

---
+++ js/konami.js
@@ -0,0 +1,51 @@
+"use strict";
+
+let code = $("#code");
+let trueCode = ""
+$(document).keyup(function (event) {
+    if (event.keyCode !== 13) {
+        trueCode += event.keyCode;
+        switch (event.key) {
+            case("ArrowUp"):
+                code.append('<img src="../assets/directionals/up.png" alt="input" width="150" height="150">');
+                break;
+            case("ArrowDown"):
+                code.append('<img src="../assets/directionals/down.png" alt="input" width="150" height="150">');
+                break;
+            case("ArrowLeft"):
+                code.append('<img src="../assets/directionals/left.png" alt="input" width="150" height="150">');
+                break;
+            case("ArrowRight"):
+                code.append('<img src="../assets/directionals/right.png" alt="input" width="150" height="150">');
+                break;
+            case("b"):
+                code.append('<img src="../assets/directionals/B.png" alt="input" width="150" height="150">');
+                break
+            case("a"):
+                code.append('<img src="../assets/directionals/A.png" alt="input" width="150" height="150">');
+                break;
+            default:
+                code.append('<img src="../assets/directionals/X.png" alt="input" width="150" height="150">');
+
+        }
+
+
+        console.log(event.key);
+    }
+});
+
+$(document).keypress(function (event) {
+    if (event.keyCode === 13) {
+        if (trueCode === "38384040373937396665") {
+            code.html("")
+            trueCode = ""
+            $('body').addClass("win").removeClass("lose")
+            alert("you have gained 30 lives!!");
+        } else {
+            code.html("")
+            trueCode = ""
+            $('body').addClass("lose").removeClass("win")
+            alert("Wrong code! try again");
+        }
+    }
+})
