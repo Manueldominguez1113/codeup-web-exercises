416b211b2cbe0704121ab2004f1dec32e42c1617
Manuel Dominguez <manuel.dominguez1113@gmail.com>
2022-02-02 11:02:27 -0600

    set up mapbox

--- js/weathermap.js
+++ js/weathermap.js
@@ -1,15 +1,15 @@
-let lat= 29.4246002;
-let lon= -98.4951405
+let lat = 29.4246002;
+let lon = -98.4951405
 
 // direct geocode to find coordinates and everything else needed.
-$.ajax(`https://api.openweathermap.org/geo/1.0/direct?q=San_antonio&limit=5&appid=`+WEATHER_CODE).done(function(geo){
+/*$.ajax(`https://api.openweathermap.org/geo/1.0/direct?q=San_antonio&limit=5&appid=`+WEATHER_CODE).done(function(geo){
     $("#location").append(geo[0].name);
     console.log(geo);
- })
+ })*/
 
-/* working weather code below. just need to .map() for each desired input and figure out how to set long&lat from the geocoder above....
-        commented out to stop calling everytime as i work on the map.
-
+// working weather code below. just need to .map() for each desired input and figure out how to set long&lat from the geocoder above....
+//        commented out to stop calling everytime as i work on the map.
+/*
 $.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&appid=${WEATHER_CODE}`).done(function (weather) {
     console.log(weather)
     const date = new Date(weather.daily[0].dt * 1000)
@@ -17,7 +17,7 @@ $.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&uni
     $("#today").append(
         "<h3>"+
         today
-       +" <br>now: "+ weather.current.temp +" mph <br>"+
+       +" <br>now: "+ weather.current.temp +" F <br>"+
        " feels like "+ weather.current.feels_like + " F <br>"+
        " high "+ weather.daily[0].temp.max +" mph <br>"+
         "low "+ weather.daily[0].temp.min+" mph <br>"+
@@ -31,7 +31,7 @@ $.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&uni
     )
     console.log("done")
 })*/
- mapboxgl.accessToken = MAPBOX_CODE;
+mapboxgl.accessToken = MAPBOX_CODE;
 const map = new mapboxgl.Map({
     container: "map",
     style: "mapbox://styles/mapbox/light-v10",
@@ -39,9 +39,40 @@ const map = new mapboxgl.Map({
     zoom: "10"
 });
 
-$("#submitMe").click(function(){
-    let input= $("#findMe").val()
-    geocode(input, MAPBOX_CODE).then(function(coordinates){
+// this helps me by showing the long lat coordinates under the mouse on the map. now to make a global variable to store the coordinate so I may use it elsewhere
+map.on('mousemove', (e) => {
+    let array;
+    array = e.lngLat.toArray();
+    document.getElementById("info").innerHTML = "<br>" +
+        " longitude: " + array[0].toFixed(2) + "<br> latitude: " + array[1].toFixed(2);
+});
+map.on('click', (e) => {
+    let long, lati, array;
+    long = document.getElementById("longFindMe");
+    lati = document.getElementById("latFindMe");
+    array = e.lngLat.toArray();
+    long.value = array[0].toFixed(2);
+    lati.value = array[1].toFixed(2);
+    let popup = new mapboxgl.Popup().setLngLat(e.lngLat).setHTML("<button id='popupBtn'>click on this bubble to know the weather here!</button>").addTo(map);
+    console.log(e.lngLat.toArray())
+    document.getElementById("snapshot").innerHTML = "<b>KA-CLICK! you're full coordinates:</b><br>" +
+        " longitude: " + array[0] + "<br> latitude: " + array[1];
+    console.log(popup)
+});
+
+//submit functions. should make latlng a basic function so i can call it either on submit or on gps.
+$("#submitMe").click(function () {
+    let input = $("#findMe").val()
+    geocode(input, MAPBOX_CODE).then(function (coordinates) {
+
+        new mapboxgl.Popup().setLngLat(coordinates).setHTML("<div class='popup-input'>"+input+"</div>").addTo(map);
+        map.setCenter(coordinates)
+        map.setZoom(16);
+    });
+})
+$("#llSubmitMe").click(function () {
+    let input = $("#findMe").val()
+    reverseGeocode(input, MAPBOX_CODE).then(function (coordinates) {
 
         let marker = new mapboxgl.Marker().setLngLat(coordinates).addTo(map).setPopup(new mapboxgl.Popup().setHTML(input));
 
@@ -49,3 +80,33 @@ $("#submitMe").click(function(){
         map.setZoom(16);
     });
 })
+
+// pixels the map pans when the up or down arrow is clicked
+const deltaDistance = 100;
+
+function easing(t) {
+    return t * (2 - t);
+}
+
+map.on('load', () => {
+    map.getCanvas().focus();
+
+    document.addEventListener('keydown', (e) => {
+
+            if (e.which === 38) {
+/* up*/           e.preventDefault();
+                map.panBy([0, -deltaDistance], {easing: easing});
+            } else if (e.which === 40) {
+/*down*/          e.preventDefault();
+                map.panBy([0, deltaDistance], {easing: easing});
+            } else if (e.which === 37) {
+/*left*/         e.preventDefault();
+                map.panBy([-deltaDistance, 0], {easing: easing});
+            } else if (e.which === 39) {
+/*right*/       e.preventDefault();
+                map.panBy([deltaDistance, 0], {easing: easing});
+            }
+        },
+        true
+    );
+});
